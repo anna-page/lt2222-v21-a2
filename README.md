22e0113075a7b138526afd02424322672ed6979c
Anna Page <guspagan@GU.GU.SE@mltgpu.flov.gu.se>
2021-03-13 20:08:52 +0100

    completed assignment

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 Assignment 2 from the course LT2222 in the University of Gothenburg's winter 2021 semester.
 
-Your name: *here*
+Your name: Anna Page
+
+## Part 5 - Evaluation
+
+- Comparing the performance on the training data against the testing data, the model is clearly overfitting. The overall performance on the test data is significantly worse.
+- There is also significant class imbalance present in the data and the model behaves accordingly. The model performs better on the larger classes and overlooks the smaller classes. For instance, 'art', 'eve', and 'nat' are represented by 53, 45, and 20 data points, respectively. By contrast, 'geo', the largest class, contains 2070 data points. The differences in test performance given this disparity are not surprised.
 
 *Answer all questions in the notebook here.  You should also write whatever high-level documentation you feel you need to here.*
+
+## Bonus Part A - Error analysis
+
+The lowest performing class on the test set is 'eve'. This class has 13 instances in the test set, of which none were correctly classified. The extremely small size of this class relative to the more populous classes is a likely cause of this poor performance. Looking at the most commonly occuring words within these 13 data points, the vast majority are grammatical ('the', 'in', 'and') and very few are lexical. While grammatical words are likely to have similar distributions across all classes, the distribution of lexical words would be more informative. The small sample size and absence of repeated lexical words in this class isn't sufficient for patterns in the data to become pronounced.
+
+## Bonus Part B - Expanding the feature space
+
+To add part of speech features to the model, I altered the Instance class to have another 'pos' attribute which contains the part of speech for each feature in the 'features' attribute. In the case of no part of speech (e.g. a word or a start/end tag), a placeholder ('<NT>') was used. From this part of speech list, an additional four features were calculated: the number of nouns, verbs, adjectives, and adverbs present in that sentence. This was used to create a second 'pos_df' which was concatenated to the original 'df'. The remainder of the process was the same: the data was split with the same random seed, a model was trained, and two confused matrices were calculated. Inspecting the matrices, the differences are minor, but it would appear that the additional features have improved model performance.
--- a2.py
+++ a2.py
@@ -15,56 +15,314 @@ import random
 # code with reasonable comments.
 
 # Function for Part 1
+import nltk 
+from nltk.stem import WordNetLemmatizer 
+nltk.download('averaged_perceptron_tagger') 
+from nltk.corpus import wordnet
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix as conf_matrix, ConfusionMatrixDisplay
+from sklearn.svm import LinearSVC
+
+lemmatizer = WordNetLemmatizer()
+
+def pos_tagger(tag): 
+    if tag.startswith('J'): 
+        return wordnet.ADJ 
+    elif tag.startswith('V'): 
+        return wordnet.VERB 
+    elif tag.startswith('N'): 
+        return wordnet.NOUN 
+    elif tag.startswith('R'): 
+        return wordnet.ADV 
+    else:           
+        return None
+
+def make_tuples(list):
+    tuples = []
+    for row in list:
+        tuple = (row[2],row[3])
+        tuples.append(tuple)
+    return tuples
+    
 def preprocess(inputfile):
-    return inputfile.readlines()
+    
+    inputdata = inputfile.readlines()
+    processed_data = []
+    for row in inputdata[1:]:
+        split_data = row.replace('\n', '')
+        listed_data = split_data.split('\t')
+        processed_data.append(listed_data)
+        
+    lowercase_lists = []
+   
+    for row in processed_data:
+        list_item = []
+        for idx, item in enumerate(row):
+            if idx != 2:
+                list_item.append(item)
+            else:
+                list_item.append(item.lower())
+        lowercase_lists.append(list_item)
+    
+    tuples = make_tuples(lowercase_lists)
+
+    wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), tuples)) 
+    
+    lemmatized_words = [] 
+    for word, tag in wordnet_tagged: 
+        if tag is None: 
+            # if there is no available tag, append the token as is 
+            lemmatized_words.append((word, '<NT>')) 
+        else:         
+            # else use the tag to lemmatize the token 
+            lemmatized_words.append((lemmatizer.lemmatize(word, tag), tag))
+
+    for idx, word_tag_tuple in enumerate(lemmatized_words):
+        lowercase_lists[idx][2] = word_tag_tuple[0]
+        lowercase_lists[idx][3] = word_tag_tuple[1]
+
+    return lowercase_lists[:]
 
 # Code for part 2
 class Instance:
-    def __init__(self, neclass, features):
+    def __init__(self, neclass, features, pos=None):
         self.neclass = neclass
         self.features = features
+        self.pos = pos
 
     def __str__(self):
-        return "Class: {} Features: {}".format(self.neclass, self.features)
+        return "Class: {} Features: {} POS: {}".format(self.neclass, self.features, self.pos)
 
     def __repr__(self):
         return str(self)
 
 def create_instances(data):
     instances = []
-    for i in range(100):
-        neclass = random.choice(['art','eve','geo','gpe','nat','org','per','tim'])
-        features = ["something"] * random.randint(1, 10)
-        instances.append(Instance(neclass, features))
+    for idx, row in enumerate(data):
+        if row[4].startswith('B'): 
+            neclass = row[4][2:]
+            features = []
+            sentence_number = row[1]
+
+            # Add preceeding features
+            for i in range(5, 0, -1):
+                if data[idx - i][1] == sentence_number:
+                    features.append(data[idx - i][2])
+
+            # Add start padding
+            if len(features) < 5:
+                start_pads = [f"<S{x}>" for x in range(5, len(features), -1)]
+                for pad in start_pads:
+                    features.insert(0, pad)
+
+            # Find end of NE
+            counter = 1
+            while data[idx + counter][4].startswith('I'):
+                 counter += 1
+            end = idx + counter - 1
+
+            # Add subsequent features
+            for i in range(1, 6):
+                try:
+                    if data[end + i][1] == sentence_number:
+                        features.append(data[end + i][2])
+                except IndexError:
+                    continue
+
+            # Add end padding
+            if len(features) < 10:
+                end_pads = [f"</S{x - 5}>" for x in range(10, len(features), -1)]
+                for pad in end_pads:
+                    features.append(pad)
+
+            instances.append(Instance(neclass, features, row[3]))
     return instances
 
 # Code for part 3
 def create_table(instances):
-    df = pd.DataFrame()
-    df['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(100)]
-    for i in range(3000):
-        df[i] = npr.random(100)
+    feature_counts = []
+    total_word_counts = {}
 
+    for instance in instances:
+        class_name = instance.neclass
+        features = instance.features
+
+        feature_word_count = {}
+
+        for word in features:
+            if word in feature_word_count:
+                feature_word_count[word] += 1
+            else:
+                feature_word_count[word] = 1
+
+        for k, v in feature_word_count.items():
+            if k in total_word_counts:
+                total_word_counts[k] += v
+            else:
+                total_word_counts[k] = v
+
+        class_and_features = {'class_name': class_name}
+        class_and_features.update(feature_word_count)
+
+        feature_counts.append(class_and_features)
+
+    top_words = sorted(total_word_counts, key=total_word_counts.get, reverse=True)[:3000]
+    df = pd.DataFrame(feature_counts).fillna(0)[['class_name'] + top_words]
+    
     return df
 
-def ttsplit(bigdf):
-    df_train = pd.DataFrame()
-    df_train['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(80)]
-    for i in range(3000):
-        df_train[i] = npr.random(80)
 
-    df_test = pd.DataFrame()
-    df_test['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(20)]
-    for i in range(3000):
-        df_test[i] = npr.random(20)
+def ttsplit(bigdf):
+    df_train = bigdf.sample(frac=0.8,random_state=200)
+    df_test = bigdf.drop(df_train.index).sample(frac=1.0)
+    df_train = df_train.reset_index(drop=True)
+    df_test = df_test.reset_index(drop=True)
         
-    return df_train.drop('class', axis=1).to_numpy(), df_train['class'], df_test.drop('class', axis=1).to_numpy(), df_test['class']
+    return (
+        df_train.drop('class_name', axis=1).to_numpy(),
+        df_train['class_name'],
+        df_test.drop('class_name', axis=1).to_numpy(),
+        df_test['class_name']
+    )
+
 
 # Code for part 5
 def confusion_matrix(truth, predictions):
-    print("I'm confusing.")
-    return "I'm confused."
+    fig, ax = plt.subplots()
+    
+    labels = truth.unique()
+    labels.sort()
+    
+    matrix = conf_matrix(truth, predictions, labels=labels)
+    
+    cm_plot = ConfusionMatrixDisplay(
+        confusion_matrix=matrix,
+        display_labels=labels,
+    )
+
+    cm_plot.plot(ax=ax)
+
+    
 
 # Code for bonus part B
-def bonusb(filename):
-    pass
+def create_instances_bonus(data):
+    pos_map = {
+        'a': '_ADJ_',
+        'n': '_NOUN_',
+        'r': '_ADV_',
+        'v': '_VERB_',
+        '<NT>': '<NT>'
+    }
+    
+    instances = []
+    for idx, row in enumerate(data):
+        if row[4].startswith('B'): 
+            neclass = row[4][2:]
+            features = []
+            pos = []
+            sentence_number = row[1]
+
+            # Add preceeding features
+            for i in range(5, 0, -1):
+                if data[idx - i][1] == sentence_number:
+                    features.append(data[idx - i][2])
+                    pos.append(pos_map[data[idx - i][3]])
+
+            # Add start padding
+            if len(features) < 5:
+                start_pads = [f"<S{x}>" for x in range(5, len(features), -1)]
+                for pad in start_pads:
+                    features.insert(0, pad)
+                    pos.insert(0, '<NT>')
+
+            # Find end of NE
+            counter = 1
+            while data[idx + counter][4].startswith('I'):
+                 counter += 1
+            end = idx + counter - 1
+
+            # Add subsequent features
+            for i in range(1, 6):
+                try:
+                    if data[end + i][1] == sentence_number:
+                        features.append(data[end + i][2])
+                        pos.append(pos_map[data[end + i][3]])
+                except IndexError:
+                    continue
+
+            # Add end padding
+            if len(features) < 10:
+                end_pads = [f"</S{x - 5}>" for x in range(10, len(features), -1)]
+                for pad in end_pads:
+                    features.append(pad)
+                    pos.append('<NT>')
+
+            instances.append(Instance(neclass, features, pos))
+    return instances
+
+
+def create_table_bonus(instances):
+    feature_counts = []
+    part_of_speech_counts = []
+    total_word_counts = {}
+
+    for instance in instances:
+        class_name = instance.neclass
+        features = instance.features
+        pos = instance.pos
+
+        feature_word_count = {}
+        pos_count = {}
+
+        for word in features:
+            if word in feature_word_count:
+                feature_word_count[word] += 1
+            else:
+                feature_word_count[word] = 1
+
+        for k, v in feature_word_count.items():
+            if k in total_word_counts:
+                total_word_counts[k] += v
+            else:
+                total_word_counts[k] = v
+
+        for p in pos:
+            if p != '<NT>':
+                if p in pos_count:
+                    pos_count[p] += 1
+                else:
+                    pos_count[p] = 1
+
+
+        class_and_features = {'class_name': class_name}
+        class_and_features.update(feature_word_count)
+
+        feature_counts.append(class_and_features)
+        part_of_speech_counts.append(pos_count)
+
+    top_words = sorted(total_word_counts, key=total_word_counts.get, reverse=True)[:3000]
+    
+    df = pd.DataFrame(feature_counts).fillna(0)[['class_name'] + top_words]
+    pos_df = pd.DataFrame(part_of_speech_counts).fillna(0)
+    
+    full_df = pd.concat([df, pos_df], axis=1)
+    
+    return full_df
+
+
+def bonusb(file):
+    input_file = open(file, 'r')
+    input_data = preprocess(input_file)
+    
+    instances = create_instances_bonus(input_data)
+    pos_df = create_table_bonus(instances)
+
+    train_X, train_y, test_X, test_y = ttsplit(pos_df)
+    
+    model = LinearSVC()
+    model.fit(train_X, train_y)
+    train_predictions = model.predict(train_X)
+    test_predictions = model.predict(test_X)
+    
+    confusion_matrix(test_y, test_predictions)
+    confusion_matrix(train_y, train_predictions)
